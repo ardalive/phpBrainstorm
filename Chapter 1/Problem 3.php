35e6ddd32304b7619e852898deaee5e11361bf8c
Victor <rikartov@gmail.com>
2021-02-23 10:41:13 +0300

    solved chapter 1 problem 3

---
+++ Chapter 1/Problem 3.php
@@ -0,0 +1,24 @@
+<?php
+$n = 1234556;
+$isIncreasingSequence = false;
+
+// Возвращает истину только если последовательность строго возрастающая.
+// Если в последовательности есть плато, возвращает ложь
+
+if($n > 11){
+    $lastDigit = $n % 10;
+    $n -= $lastDigit;
+    $n /= 10;
+    $notIncreaseCounter = 0;
+
+    while($n > 0){
+        $currentDigit = $n % 10;
+        $n -= $currentDigit;
+        $n /= 10;
+        if($lastDigit <= $currentDigit) $notIncreaseCounter++;
+        $lastDigit = $currentDigit;
+    }
+    if($notIncreaseCounter < 1) $isIncreasingSequence = true;
+}
+
+echo $isIncreasingSequence === true ? 'true' : 'false';
